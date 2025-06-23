e2cae49c16f22d64060a8c812cd677651be2afc9
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-06-23 12:04:57 +0500

    app setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Button } from "./components/ui/button";
+
+import { increment, decrement } from "./redux/slices/counterSlice";
 
 const App = () => {
-  return <div>this is the app</div>;
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="flex items-center gap-2">
+      <Button onClick={() => dispatch(decrement())}>Decrement</Button>
+      <h1>{count}</h1>
+      <Button onClick={() => dispatch(increment())}>Increment</Button>
+    </div>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,16 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import { store } from "./redux/store/store.js";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+      <Toaster />
+    </BrowserRouter>
+  </Provider>
+);
