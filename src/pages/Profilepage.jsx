ce10f5a02e55518838b26e6dd91321cfa1edd817
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-06-25 21:12:18 +0500

    implement the change name and image only in state

--- src/pages/Profilepage.jsx
+++ src/pages/Profilepage.jsx
@@ -4,11 +4,11 @@ import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
 import { Button } from "../components/ui/button";
 
-import { Pencil } from "lucide-react";
+import { Pencil, Camera } from "lucide-react";
 import toast from "react-hot-toast";
 import { useSelector, useDispatch } from "react-redux";
 
-import { setName } from "../redux/slices/profileSlice";
+import { setName, setProfilePic } from "../redux/slices/profileSlice";
 
 const Profilepage = () => {
   const { name, email, profilePic, role } = useSelector(
@@ -18,32 +18,52 @@ const Profilepage = () => {
 
   const [edit, setEdit] = useState(false);
   const [newName, setNewName] = useState(name);
+  const [newProfilePic, setNewProfilePic] = useState(profilePic);
 
   const handleClick = () => {
     setEdit((prev) => !prev);
     document.getElementById("name").focus();
   };
 
+  const handleChange = (e) => {
+    const url = URL.createObjectURL(e.target.files[0]);
+    setNewProfilePic(url);
+  };
+
   const handleSubmit = () => {
-    if (newName === "") {
-      return toast.error("Name cannot be empty");
+    if (newName === "" && !newProfilePic) {
+      return toast.error("Invalid changes");
     } else {
       setEdit(false);
       dispatch(setName(newName));
+      dispatch(setProfilePic(newProfilePic));
       localStorage.setItem("name", JSON.stringify(newName));
+      localStorage.setItem("profilePic", JSON.stringify(newProfilePic));
       toast.success("Saved changes");
     }
   };
 
-  // Todo: upload image button and functionality and api call to the backend
+  // Todo: api call to the backend
 
   return (
     <div className="min-h-full w-full px-2 flex flex-col gap-2">
       <h2 className="text-2xl font-semibold">Profile</h2>
       <div className="flex flex-col gap-2 items-center md:items-start">
-        <Avatar className={"size-48"}>
+        <Avatar className={"relative size-48 overflow-visible"}>
+          <Label htmlFor={"image"} className={"absolute top-32 right-0 z-50"}>
+            <Camera
+              className="size-12 bg-primary dark:text-black p-3 rounded-full cursor-pointer"
+              size={"28"}
+            />
+          </Label>
+          <Input
+            className={"hidden"}
+            id="image"
+            type={"file"}
+            onChange={handleChange}
+          />
           <AvatarImage
-            className={"object-cover"}
+            className={"object-cover rounded-full"}
             src={profilePic}
           ></AvatarImage>
           <AvatarFallback className={"text-9xl bg-zinc-900 text-white"}>
@@ -72,7 +92,7 @@ const Profilepage = () => {
           <Input
             id={"email"}
             type={"text"}
-            readonly={"true"}
+            readOnly={true}
             value={email}
             className={"w-fit"}
           />
@@ -82,17 +102,16 @@ const Profilepage = () => {
           <Input
             id={"role"}
             type={"text"}
-            readonly={"true"}
+            readOnly={true}
             value={role}
             className={"w-fit"}
           />
         </div>
         <Button
           onClick={handleSubmit}
-          disabled={!edit}
-          className={"bg-primary px-16 md:px-12 cursor-pointer"}
+          className={"bg-primary mt-2 px-24 md:px-20 cursor-pointer"}
         >
-          Save changes
+          Save
         </Button>
       </div>
     </div>
