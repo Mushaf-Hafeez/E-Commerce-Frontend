9f6a2ff5b44a06eae87715aee02bef0773fd0768
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2026-03-14 13:47:18 +0500

    design hero section

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,35 +1,7 @@
-import { Button } from "../components/ui/button";
-import { Link } from "react-router-dom";
-import { ArrowRight } from "lucide-react";
-
 const Homepage = () => {
   return (
     <section className="h-[90vh] w-full flex items-center justify-center">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <Button
-          className={
-            "bg-zinc-200 hover:bg-zinc-300 text-zinc-900 dark:text-zinc-100 dark:bg-zinc-900 dark:hover:bg-zinc-800 rounded-full"
-          }
-          asChild
-        >
-          <Link to={"/products"}>
-            Browse products <ArrowRight />
-          </Link>
-        </Button>
-        <h2 className="text-primary text-4xl font-semibold">
-          Built to Perform. Designed for Precision.
-        </h2>
-        <p className="w-5/12 text-center">
-          Shop premium keyboards, mouses, and mousepads — engineered for
-          comfort, speed, and accuracy. Sign in to explore curated collections
-          and personalized deals.
-        </p>
-        <div className="flex items-center gap-2">
-          <Button className={"font-semibold"} asChild>
-            <Link to="/login">Get Started</Link>
-          </Button>
-        </div>
-      </div>
+      This is the hero section of the website
     </section>
   );
 };
